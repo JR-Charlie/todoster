7e6916d5f3d08f1a2342d68d725ffcc0c28e3d59
Charlie <charlie@myjobreel.com>
2017-03-13 22:40:35 +0000

    Add strikethough

--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -1,11 +1,12 @@
 $(function() {
-
   // The taskHtml method takes in a JavaScript representation
   // of the task and produces an HTML representation using
   // <li> tags
   function taskHtml(task) {
     var checkedStatus = task.done ? "checked" : "";
-    var liElement = '<li><div class="view"><input class="toggle" type="checkbox"' +
+    var liClass = task.done ? "completed" : "";
+    var liElement = '<li id="listItem-' + task.id +'" class="' + liClass + '">' +
+    '<div class="view"><input class="toggle" type="checkbox"' +
       " data-id='" + task.id + "'" +
       checkedStatus +
       '><label>' +
@@ -15,7 +16,7 @@ $(function() {
     return liElement;
   }
 
-  // toggleTask takes in an HTML representation of
+  // toggleTask takes in an HTML representation of the
   // an event that fires from an HTML representation of
   // the toggle checkbox and  performs an API request to toggle
   // the value of the `done` field
@@ -29,22 +30,28 @@ $(function() {
       task: {
         done: doneValue
       }
-    });
+    }).success(function(data) {
+      var liHtml = taskHtml(data);
+      var $li = $("#listItem-" + data.id);
+      $li.replaceWith(liHtml);
+      $('.toggle').change(toggleTask);
+
+    } );
   }
 
   $.get("/tasks").success( function( data ) {
     var htmlString = "";
 
     $.each(data, function(index,  task) {
-
       htmlString += taskHtml(task);
     });
     var ulTodos = $('.todo-list');
     ulTodos.html(htmlString);
 
-    $('.toggle').change(toggleTask)
+    $('.toggle').change(toggleTask);
 
   });
+
 
   $('#new-form').submit(function(event) {
     event.preventDefault();
@@ -59,7 +66,6 @@ $(function() {
       var ulTodos = $('.todo-list');
       ulTodos.append(htmlString);
       $('.toggle').click(toggleTask);
-      $('.new-todo').val('');
     });
   });
 
